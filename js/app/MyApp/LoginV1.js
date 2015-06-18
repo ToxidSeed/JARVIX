ce66cdec06ac54877e0da9da992c5cf6e255ad2c
ToxidSeed <maccgeo@hotmail.com>
2015-06-18 17:59:51 -0500

    Cambios Angular JS
    Cambios Angular JS

---
+++ js/app/MyApp/LoginV1.js
@@ -0,0 +1,21 @@
+angular.module('app',[]).controller('Login',function($scope,$http){
+    $scope.login = function(){
+
+        $http({
+            method: 'POST',
+            url: '/endpoint',
+            data: {
+                    email : $scope.email,
+                    password : $scope.password
+                }
+            }).
+        success(function(data, status, headers, config) {
+        // called when http call completes successfully
+        }).
+        error(function(error, status, headers, config) {
+        // called when the http call fails.
+        // The
+        });                        
+    }
+});
+    
